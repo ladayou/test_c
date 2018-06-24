7472135ec8be29b085b584fd543e68d743f202f3
haozi <1351135881@qq.com>
2018-06-24 15:32:29 +0800

    Update a.cpp
    上一个版本有问题。。。

--- a.cpp
+++ a.cpp
@@ -1,23 +1,24 @@
+#include <stdio.h>
+ 
 main()
-
 {
-
-int i,j,k;
-
-printf("\n");
-
-for(i=1;i<5;i++)　　　　／*以下为三重循环*/
-
-　for(j=1;j<5;j++)
-
-　　for (k=1;k<5;k++)
-
-　　　{
-
-　　　　if (i!=k&&i!=j&&j!=k) /*确保i、j、k三位互不相同*/
-
-　　　　printf("%d,%d,%d\n",i,j,k);
-
-　　　}
-
+ int a = 21;
+ int b = 10;
+ int c ;
+ 
+ c = a + b;
+ printf("Line 1 - c 的值是 %d\n", c );
+ c = a - b;
+ printf("Line 2 - c 的值是 %d\n", c );
+ c = a * b;
+ printf("Line 3 - c 的值是 %d\n", c );
+ c = a / b;
+ printf("Line 4 - c 的值是 %d\n", c );
+ c = a % b;
+ printf("Line 5 - c 的值是 %d\n", c );
+ c = a++; 
+ printf("Line 6 - c 的值是 %d\n", c );
+ c = a--; 
+ printf("Line 7 - c 的值是 %d\n", c );
+ 
 }
